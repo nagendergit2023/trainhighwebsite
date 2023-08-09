60a93ec3d041b61c3fd7bfa2862a16223d435b45
nagendergit2023 <136920949+nagendergit2023@users.noreply.github.com>
2023-08-10 00:46:58 +0530

    Coming soon page changes

--- src/Pages/Home/ComingSoon.js
+++ src/Pages/Home/ComingSoon.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import "./ComingSoon.css";
 import TrainHighGymLogo from "../../assets/images/train_high_gym_logo.png";
+import { BiLogoFacebook, BiLogoWhatsapp, BiPhoneCall, BiLogoInstagram } from 'react-icons/bi';
 
 function ComingSoon() {
   return (
     <section className="comingsoon_screen">
       <img src={TrainHighGymLogo} className="main_title img-fluid w-25 rounded-circle infinite-flip" />
-        <p className="main_title">ARE YOU READY?</p>
-        {/* <p className="main_title">JANAKPURI</p> */}
+        <p className="main_title">OPENING SOON!</p>
+        <div className="d-flex align-items-center gap-4 position-relative z-1 ">
+          <a href="https://instagram.com/trainhighgym?igshid=NjIwNzIyMDk2Mg==" className="social-icon-wrapper text-white" target="_blank">
+          <BiLogoInstagram className="social-icon" /> 
+          </a>
+          {/* <a href="" className="social-icon-wrapper text-white">
+          <BiLogoFacebook className="social-icon" /> 
+          </a>
+          <a href="" className="social-icon-wrapper text-white">
+          <BiPhoneCall className="social-icon" /> 
+          </a>
+          <a href="" className="social-icon-wrapper text-white">
+          <BiLogoWhatsapp className="social-icon" /> 
+          </a> */}
+        </div>
         <div className="dark_overlay"></div>
     </section>
   )
