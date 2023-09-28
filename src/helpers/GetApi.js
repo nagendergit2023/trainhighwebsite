a9ea852798a067416b38f8bbfdf7db1cf024ae44
manmeet <manmeetsehgal338@gmail,com>
2023-09-28 13:13:15 +0530

    commit

--- src/helpers/GetApi.js
+++ src/helpers/GetApi.js
@@ -1,4 +1,4 @@
-var urlString = "http://68.178.170.174:3309/trainhighgym-api/";
+var urlString = "http://trainhighgym.com:3307/trainhighgym-api/";
 // var urlString = "http://localhost:3307/trainhighgym-api/";
 var access = sessionStorage.getItem("access");
 const GetApiCall = {
