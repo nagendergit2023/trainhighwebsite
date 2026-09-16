4d2e3de9e369dd92c69be0be6c52220d6b725b08
nagendergit2023 <nagenderpalsingh2012@gmail.com>
2026-09-16 17:43:06 +0530

    event gallery page changes by Nagender

--- src/Pages/Training/GroupClasses.js
+++ src/Pages/Training/GroupClasses.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import Hero from '../../Components/Hero/Hero.js';
+import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
--- src/Pages/Training/Pilates.js
+++ src/Pages/Training/Pilates.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React from 'react';
 import { Container } from 'react-bootstrap';
-import Hero from '../../Components/Hero/Hero.js';
 import { Row } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
 import PilatesTraining from "../../assets/images/trainings/pilates.jpg";
